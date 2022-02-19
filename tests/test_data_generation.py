eb760e986024102ff65da10bd3f751f6a9e0e3fd
Till Hoffmann <tillahoffmann@gmail.com>
2022-02-18 20:17:34 -0500

    Fix test name.

--- tests/test_data_generation.py
+++ tests/test_data_generation.py
@@ -6,7 +6,7 @@ def test_BA_ref_table():
     model_index, summaries, is_discrete, times = dg.BA_ref_table(10, 20)
 
 
-def test():
+def test_DMC_DMR_ref_table():
     seed_network = nx.Graph()
     seed_network.add_node(0)
     model_index, summaries, is_discrete, times = dg.DMC_DMR_ref_table(seed_network, 10, 20)
