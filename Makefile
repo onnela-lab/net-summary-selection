8d75b517f8a679141d52fb9e4237025617ace79f
Till Hoffmann <tillahoffmann@gmail.com>
2022-02-24 14:30:41 -0500

    Add code and targets to generate reference tables.

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-.PHONY : build sync tests
+.SECONDEXPANSION :
+.PHONY : build sync tests workspace/simulations workspace/simulations/ba
+
+# Helper function to split by special character.
+wordx = $(word $2,$(subst $3, ,$1))
 
 requirements.txt : requirements.in setup.py
 	pip-compile -v
@@ -10,3 +14,51 @@ tests :
 	pytest -v -s --cov=cost_based_selection --cov-report=html --cov-report=term
 
 build : tests
+
+# Simulation
+# ==========
+
+# General configuration
+# ---------------------
+
+SIMULATION_ROOT = workspace/simulations
+NUM_NODES = 1000
+BATCH_SIZE = 100
+NUM_BATCHES_train = 50
+NUM_BATCHES_test = 50
+BATCH_INDICES_train = $(shell seq ${NUM_BATCHES_train})
+BATCH_INDICES_test = $(shell seq ${NUM_BATCHES_test})
+SEED_train =
+SEED_test = 999
+MODELS = ba dmX
+SPLITS = train test
+
+# Targets for different models
+# ----------------------------
+
+SIMULATIONS_ba_train = $(addprefix ${SIMULATION_ROOT}/ba/train/,${BATCH_INDICES_train:=.pkl})
+SIMULATIONS_ba_test = $(addprefix ${SIMULATION_ROOT}/ba/test/,${BATCH_INDICES_test:=.pkl})
+SIMULATIONS_ba = ${SIMULATIONS_ba_train} ${SIMULATIONS_ba_test}
+
+SIMULATIONS_dmX_train = $(addprefix ${SIMULATION_ROOT}/dmX/train/,${BATCH_INDICES_train:=.pkl})
+SIMULATIONS_dmX_test = $(addprefix ${SIMULATION_ROOT}/dmX/test/,${BATCH_INDICES_test:=.pkl})
+SIMULATIONS_dmX = ${SIMULATIONS_dmX_train} ${SIMULATIONS_dmX_test}
+
+SIMULATIONS = ${SIMULATIONS_ba} ${SIMULATIONS_dmX}
+
+# The pattern will be {model}/{split}/{batch}
+${SIMULATIONS} : ${SIMULATION_ROOT}/%.pkl : scripts/generate_reference_table.py
+	NUMEXPR_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 python $< \
+		--seed=${SEED_$(call wordx,$*,2,/)}$(call wordx,$*,3,/) $(call wordx,$*,1,/) ${NUM_NODES} \
+		${BATCH_SIZE} $@
+
+# Grouped targets
+# ---------------
+
+SIMULATIONS_models = $(addprefix ${SIMULATION_ROOT}/,${MODELS})
+SIMULATIONS_models_splits = $(foreach m,${MODELS},$(foreach s,${SPLITS},${SIMULATION_ROOT}/${m}/${s}))
+${SIMULATION_ROOT} : ${SIMULATIONS_models}
+${SIMULATIONS_models} : ${SIMULATION_ROOT}/% : $$(addprefix $$@/,${SPLITS})
+${SIMULATIONS_models_splits} : ${SIMULATION_ROOT}/% : \
+	$$(addprefix $$@/,$${BATCH_INDICES_$$(call wordx,$$*,2,/):=.pkl})
+
---
+++ scripts/generate_reference_table.py
@@ -0,0 +1,84 @@
+import argparse
+import contextlib
+import logging
+import networkx as nx
+import numpy as np
+import os
+import pickle
+import time
+from tqdm import tqdm
+from cost_based_selection import data_generation
+from cost_based_selection.summaries2 import compute_summaries
+
+
+logging.basicConfig(level='INFO')
+
+
+@contextlib.contextmanager
+def log_time(logger: logging.Logger, message: str, **kwargs):
+    start = time.time()
+    yield
+    kwargs['time'] = time.time() - start
+    logger.info(message, kwargs)
+
+
+def __main__():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', '-s', help='random number generator seed', type=int)
+    parser.add_argument('--show_progress', '-p', help='show a progress bar', action='store_true')
+    parser.add_argument('model', help='model to simulate from', choices=['ba', 'dmX'])
+    parser.add_argument('num_nodes', type=int, help='number of nodes in the graph')
+    parser.add_argument('num_samples', type=int, help='number of samples to generate')
+    parser.add_argument('output', help='output path for the reference table')
+    args = parser.parse_args()
+
+    logger = logging.getLogger('generate_reference_table')
+
+    if args.seed is not None:
+        np.random.seed(args.seed)
+
+    result = {
+        'args': vars(args),
+        'start': time.time(),
+    }
+
+    steps = range(args.num_samples)
+    if args.show_progress:
+        steps = tqdm(steps)
+    for step in steps:
+        # Generate a graph.
+        with log_time(logger, 'generated %(model)s graph in %(time).3fs', model=args.model):
+            if args.model == 'ba':
+                model_label = 1 + np.random.randint(4)
+                graph = nx.barabasi_albert_graph(args.num_nodes, model_label)
+            elif args.model == 'dmX':
+                seed_network = nx.Graph()
+                seed_network.add_edge(0, 1)
+                qs = np.random.uniform(0.25, 0.75, 2)
+                model_label = np.random.choice(['dmc', 'dmr'])
+                if model_label == 'dmc':
+                    graph = data_generation.DMC(seed_network, args.num_nodes, *qs)
+                elif model_label == 'dmr':
+                    graph = data_generation.DMR(seed_network, args.num_nodes, *qs)
+                else:
+                    raise ValueError(model_label)
+            else:
+                raise ValueError(args.model)
+            result.setdefault('model_labels', []).append(model_label)
+
+        # Evaluate the summary statistics.
+        with log_time(logger, 'computed summaries for %(model)s graph in %(time).3fs',
+                      model=model_label):
+            result.setdefault('summaries', []).append(compute_summaries(graph, True).results)
+        logger.info('computed summary statistics for %d of %d %s graphs (%.1f%%)', step + 1,
+                    args.num_samples, args.model, 100 * (step + 1) / args.num_samples)
+
+    result['end'] = time.time()
+    directory = os.path.dirname(args.output)
+    os.makedirs(directory, exist_ok=True)
+    with open(args.output, 'wb') as fp:
+        pickle.dump(result, fp)
+
+
+if __name__ == '__main__':
+    __main__()
