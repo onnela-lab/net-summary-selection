a67c9367920d63035d5d16a8280c605bc64421cb
Till Hoffmann <tillahoffmann@gmail.com>
2022-03-31 14:17:38 -0400

    Drop multi_ versions of methods because we can multiprocess on the CLI.

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -3,6 +3,7 @@ from cost_based_selection import cost_based_methods
 from cost_based_selection import data_generation
 from cost_based_selection import preprocessing_utils
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -11,7 +12,7 @@ def test_smoke():
     """
     This test reproduces the README code.
     """
-    num_sims_per_model = 50
+    num_sims_per_model = 10
     num_nodes = 20
 
     # Generate data.
@@ -31,11 +32,11 @@ def test_smoke():
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.5, stratify=y)
 
     # Apply the JMI method over a grid.
-    grid_cost_param = [0, 1, 2, 3, 4]
-    ranks = cost_based_methods.multi_JMI(
-        X=X_train, y=y_train, is_disc=is_discrete, cost_vec=average_costs,
-        cost_param_vec=grid_cost_param,
-    )
+    result = pd.DataFrame({"cost_param": [0, 1, 2]})
+    rankings = [cost_based_methods.JMI(
+            X=X_train, y=y_train, is_disc=is_discrete, cost_vec=average_costs, cost_param=cost_param
+        )[0] for cost_param in result.cost_param]
+    result = pd.concat([result, pd.DataFrame(rankings, columns=1 + np.arange(X.shape[1]))], axis=1)
 
     # Run cross validation for model classification.
     num_statistics = 15
@@ -44,7 +45,7 @@ def test_smoke():
 
     avg_accuracy, std_accuracy, total_cost, prop_noise = \
         cost_based_analysis.accuracy_classifier_plot(
-            dfPen_Ranking=ranks, X_val=X_val, y_val=y_val, cost_vec=average_costs,
+            dfPen_Ranking=result, X_val=X_val, y_val=y_val, cost_vec=average_costs,
             noise_idx=noise_indices, subset_size=num_statistics, classifier_func=classifier,
             args_classifier=kwargs, num_fold=3, save_name=None,
         )
