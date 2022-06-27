7c47e60c75d64cc2b6f352fcf415a79d6cec557d
Till Hoffmann <tillahoffmann@gmail.com>
2022-06-27 15:52:49 -0400

    Use dedicated base names and additional penalties.

--- dodo.py
+++ dodo.py
@@ -4,7 +4,6 @@ import itertools as it
 import numpy as np
 import os
 import pathlib
-import re
 import sys
 
 
@@ -45,114 +44,130 @@ CONFIGS = {
         "seed_offset": 999,
     },
 }
-for num_nodes in (1 + np.arange(10)) * 100:
-    CONFIGS[f"n{num_nodes}"] = {
+
+# Generate `num_seeds` different versions of the pilot rankings.
+num_seeds = 5
+for num_nodes, seed in it.product((1 + np.arange(10)) * 100, range(num_seeds)):
+    CONFIGS[f"pilot/num_nodes-{num_nodes}/seed-{seed}"] = {
         "num_nodes": num_nodes,
-        "seed_offset": num_nodes * 31,
+        "seed_offset": num_nodes * 31 + 7919 * seed,
     }
 
 # Different methods to generate rankings from.
 METHODS = [
-    "JMI", "JMIM", "mRMR", "reliefF_distance", "reliefF_rf_prox", "random_ranking",
+    "JMI", "JMIM", "mRMR", "reliefF_l1", "reliefF_rf", "random_ranking",
     "pen_rf_importance_impurity", "pen_rf_importance_permutation",
     "weighted_rf_importance_impurity", "weighted_rf_importance_permutation",
 ]
 # Cost regularization values.
 PENALTIES = [
     0, 0.0125, 0.025, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.1, 1.6, 2.3, 3.2, 4.5, 6.4, 8, 9, 10, 11, 12,
-    12.8, 25.6, 51.2,
+    12.8, 25.6, 51.2, 102.4, 204.8,
 ]
 
 
+# Generate all tasks within one function. This is somewhat unconventional but avoids having repeated
+# loops in different tasks.
 def task_main():
-    """
-    Generate all tasks within one function. This is somewhat unconventional but avoids having
-    repeated loops in different tasks.
-    """
     simulation_script = "scripts/generate_reference_table.py"
     ranking_script = "scripts/rank_features.py"
     mi_script = "scripts/evaluate_mutual_information.py"
     eval_script = "scripts/evaluate_ranked_features.py"
 
-    for (key, config), model in it.product(CONFIGS.items(), MODELS):
-        # Generate realisations of different models with varying number of nodes.
-        simulation_targets = []
-        for batch in range(NUM_BATCHES):
-            seed = config["seed_offset"] + batch
-            basename = os.path.join(model, "simulations", key)
-            name = f"batch-{batch}"
-            target = ROOT / basename / (name + ".pkl")
-            action = cmd_action([PYTHON, simulation_script, f"--seed={seed}", model,
-                                str(config["num_nodes"]), str(BATCH_SIZE), target])
+    for model in MODELS:
+        configs_by_seed = {}
+        for key, config in CONFIGS.items():
+            simulation_basename = os.path.join(model, "simulations", key)
+            # Generate realisations of different models with varying number of nodes.
+            simulation_targets = []
+            for batch in range(NUM_BATCHES):
+                seed = config["seed_offset"] + batch
+                configs_by_seed.setdefault(seed, []).append(f"{key}/batch-{batch}")
+                name = f"batch-{batch}"
+                target = ROOT / simulation_basename / (name + ".pkl")
+                action = cmd_action([PYTHON, simulation_script, f"--seed={seed}", model,
+                                    str(config["num_nodes"]), str(BATCH_SIZE), target])
+                yield default_task | {
+                    "basename": simulation_basename,
+                    "name": name,
+                    "actions": [action],
+                    "file_dep": [simulation_script],
+                    "targets": [target],
+                    "doc": f"simulation for {model} models with {config['num_nodes']} (batch "
+                           f"{batch})",
+                }
+                simulation_targets.append(target)
+
+            # No need to rank the test set.
+            if key == "test":
+                continue
+
+            # Precompute the (conditional) mutual information between features.
+            mi_basename = os.path.join(model, "rankings", key)
+            name = "mutual_information"
+            mi_target = ROOT / mi_basename / (name + ".pkl")
+            action = cmd_action([PYTHON, mi_script, "--adjusted", mi_target, *simulation_targets])
             yield default_task | {
-                "basename": basename,
+                "basename": mi_basename,
                 "name": name,
                 "actions": [action],
-                "file_dep": [simulation_script],
-                "targets": [target],
+                "file_dep": simulation_targets,
+                "targets": [mi_target],
             }
-            simulation_targets.append(target)
 
-        # No need to rank the test set.
-        if key == "test":
-            continue
+            # Rank features using different methods for different configurations. We only use
+            # unpenalized methods for comparing between features selected based on varying number of
+            # nodes.
+            penalties = [0.] if key.startswith("pilot") else PENALTIES
+            for method in METHODS:
+                ranking_basename = os.path.join(model, "rankings", key, method)
+                ranking_targets = []
+                for penalty in penalties:
+                    # Building the trees for the weighted RF penalization takes AGES if the penalty
+                    # is large. So we just skip large penalties here.
+                    if method.startswith("weighted_rf_importance") and penalty > 100:
+                        continue
 
-        # Precompute the (conditional) mutual information between features.
-        basename = os.path.join(model, "rankings", key)
-        name = "mutual_information"
-        mi_target = ROOT / basename / (name + ".pkl")
-        action = cmd_action([PYTHON, mi_script, "--adjusted", mi_target, *simulation_targets])
-        yield default_task | {
-            "basename": basename,
-            "name": name,
-            "actions": [action],
-            "file_dep": simulation_targets,
-            "targets": [mi_target],
-        }
+                    name = f"penalty-{penalty}"
+                    target = ROOT / ranking_basename / (name + ".pkl")
 
-        # Rank features using different methods for different configurations. We only use
-        # unpenalized methods for comparing between features selected based on varying number of
-        # nodes.
-        penalties = [0.] if re.match(r"n\d+", key) else PENALTIES
-        for method in METHODS:
-            basename = os.path.join(model, "rankings", key, method)
-            ranking_targets = []
-            for penalty in penalties:
-                name = f"penalty-{penalty}"
-                target = ROOT / basename / (name + ".pkl")
+                    # Construct arguments for the ranking of features.
+                    file_dep = list(simulation_targets)
+                    args = [PYTHON, ranking_script, '--adjusted', f'--penalty={penalty}', method,
+                            target]
+                    if method in ['JMI', 'JMIM', 'mRMR']:
+                        args.append(f'--mi={mi_target}')
+                        file_dep.append(mi_target)
+                    args = args + simulation_targets
 
-                # Construct arguments for the ranking of features.
-                file_dep = list(simulation_targets)
-                args = [PYTHON, ranking_script, '--adjusted', f'--penalty={penalty}', method,
-                        target]
-                if method in ['JMI', 'JMIM', 'mRMR']:
-                    args.append(f'--mi={mi_target}')
-                    file_dep.append(mi_target)
-                args = args + simulation_targets
+                    yield default_task | {
+                        "basename": ranking_basename,
+                        "actions": [cmd_action(args)],
+                        "targets": [target],
+                        "name": name,
+                        "file_dep": file_dep,
+                    }
+                    ranking_targets.append(target)
+
+                # Evaluate ranked features on the test set.
+                evaluation_basename = os.path.join(model, "evaluation", key)
+                target = ROOT / evaluation_basename / f"{method}.pkl"
+                action = cmd_action([PYTHON, eval_script, "--model=KNN", ROOT / ranking_basename,
+                                    ROOT / model / "simulations" / "test", target])
+                # Construct dependencies which comprises rankings and the simulations of the test
+                # set.
+                file_dep = list(ranking_targets)
+                file_dep.extend([ROOT / model / "simulations" / "test" / f"batch-{batch}.pkl" for
+                                 batch in range(NUM_BATCHES)])
 
                 yield default_task | {
-                    "basename": basename,
-                    "actions": [cmd_action(args)],
+                    "basename": evaluation_basename,
+                    "name": method,
                     "targets": [target],
-                    "name": name,
+                    "actions": [action],
                     "file_dep": file_dep,
                 }
-                ranking_targets.append(target)
 
-            # Evaluate ranked features on the test set.
-            name = "evaluation"
-            target = ROOT / basename / (name + ".pkl")
-            action = cmd_action([PYTHON, eval_script, "--model=KNN", ROOT / basename,
-                                 ROOT / model / "simulations" / "test", target])
-            # Construct dependencies which comprises rankings and the simulations of the test set.
-            file_dep = list(ranking_targets)
-            file_dep.extend([ROOT / model / "simulations" / "test" / f"batch-{batch}.pkl" for batch
-                             in range(NUM_BATCHES)])
-
-            yield default_task | {
-                "basename": basename,
-                "name": name,
-                "targets": [target],
-                "actions": [action],
-                "file_dep": file_dep,
-            }
+        for seed, configs in configs_by_seed.items():
+            if len(configs) > 1:
+                raise ValueError(f"configs {configs} have the same seed {seed}")
